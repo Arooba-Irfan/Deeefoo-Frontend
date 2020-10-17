c941254bb14279b68288d6ae08bfa0506e583749
laiba166-shaikh <laibas166@gmail.com>
2020-10-17 05:09:10 +0500

    check req

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {AppLoading} from 'expo';
+import { StyleSheet, View, Image } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={{flex:1,borderWidth:2,borderColor:'#000'}}>
+      <Image source={require('./assets/curve.png')}  style={styles.container}/>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,9 +14,12 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    height:'40%',
+    width:'50%',
+    position:'absolute',
+    top:0,
+    right:0,
+    // justifyContent:'center',
+    // alignItems:'center'
   },
 });
